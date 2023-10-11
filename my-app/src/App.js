f404389d64fde96f96264383fe6d14bc8e749a31
Debbie Yuen <deborahyuen2016@berkeley.edu>
2023-10-11 03:18:48 -0700

    unity webgl to react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import { Unity, useUnityContext } from "react-unity-webgl";
+import React from "react";
+// import '../public/Build/build_2.framework'
+import Unity, { UnityContext } from "react-unity-webgl";
+// import { Unity, useUnityContext } from "react-unity-webgl";
+// import { Unity, useUnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "./build_4/Build/build_4.loader.js",
+  dataUrl: "./build_4/Build/build_4.data",
+  frameworkUrl: "./build_4/Build/build_4.framework.js",
+  codeUrl: "./build_4/Build/build_4.wasm",
+});
 
 function App() {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "build/myunityapp.loader.js",
-    dataUrl: "build/myunityapp.data",
-    frameworkUrl: "build/myunityapp.framework.js",
-    codeUrl: "build/myunityapp.wasm",
-  });
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Unity unityProvider={unityProvider} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>hi debbie</h1> */}
+      {/* <div>
+        <header>Octocat</header>
+        <button>Page 1</button>
+      </div> */}
+      
+      
+      <div className ="Unity">
+        <Unity unityContext={unityContext} 
+        style= {{
+          width: "100%",
+          justifySelf: "center",
+          alignSelf: "center",
+          ovrflow: "hidden",
+        }}/>  
+      </div>
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Unity unityProvider={unityProvider} />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
